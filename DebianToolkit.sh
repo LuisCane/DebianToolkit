4df5e0dc14a414f99ae750c092b50e758c92b011
Luis Cane aka Gabe <84547909+LuisCane@users.noreply.github.com>
2023-01-10 17:05:57 -0500

    in progress

--- DebianToolkit.sh
+++ DebianToolkit.sh
@@ -15,25 +15,205 @@ nc="\e[0m"
 si=✔
 no=✘
 
-function checkroot(){
-	     echo ""
-	     echo -e "$a check root user $nc"
-	     sleep 4
-    if [ "$(id -u)" == "0" ]; then
-	     echo ""
-	     echo -e " $b[$v$si$b] root $nc"
-	     sleep 4
-	     echo ""
+# This is a general-purpose function to ask Yes/No questions in Bash, either
+# with or without a default answer. It keeps repeating the question until it
+# gets a valid answer.
+ask() {
+  #printf '\n--> Function: %s <--\n' "${FUNCNAME[0]}"
+  # https://djm.me/ask
+  local prompt default reply
+
+  while true; do
+
+    if [[ "${2:-}" == "Y" ]]; then
+      prompt="[Y/n]"
+      default=Y
+    elif [[ "${2:-}" == "N" ]]; then
+      prompt="[y/N]"
+      default=N
     else
-             echo ""
-	     echo -e " $b[$r$no$b] root $nc"
-	     sleep 4
-	     echo ""
-	     echo -e "$r EXITING $nc"
-	     sleep 4
-	     echo ""
+      prompt="[y/n]"
+      default=
+    fi
+
+    # Ask the question (not using "read -p" as it uses stderr not stdout)
+    printf '\n'
+    printf '%s ' $1 $prompt
+
+    read reply
+
+    # Default?
+    if [[ -z "$reply" ]]; then
+      reply=${default}
+    fi
+
+    # Check if the reply is valid
+    case "$reply" in
+    Y* | y*) return 0 ;;
+    N* | n*) return 1 ;;
+    esac
+
+  done
+}
+
+#Check if User is Root.
+IsRoot() {
+    if [[ $EUID = 0 ]]; then
+      return 0
+      else
+      return 1
+    fi
+}
+
+function checkroot(){
+	     printf "\n$a check root user $nc"
+	     sleep 1
+    if [ IsRoot ]; then
+	     printf "\n $b[$v$si$b] root $nc\n"
+	     sleep 1
+    else
+             printf "\n $b[$r$no$b] root $nc"
+	     sleep 1
+	     printf "\n$r EXITING $nc\n"
+	     sleep 1
 	     exit
     fi	
+}
+
+#Check if Package is installed
+CheckForPackage() {
+    return $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed")
+}
+
+#Install specified Package
+InstallPKG() {
+    $PKGMGR install -y $1
+    check_exit_status;
+}
+#Remove specified Package
+RemovePKG() {
+    $PKGMGR remove -y $1
+    check_exit_status;
+}
+
+UpdateSoftware() {
+    if IsRoot; then
+        printf '\nUpdating Software.\nNote: To Update Flatpak software, run this script without root or sudo.\n'
+        UpdateApt
+        UpdateSnap;
+    elif ! CheckForPackage flatpak; then
+      UpdateFlatpak;
+    else   
+      printf '\nSkipping Updates.\n'
+    fi
+}
+
+
+#Update and upgrade apt packages repos
+UpdateApt () {
+    if ask "Would you like to update the apt repositories?" Y; then
+        $PKGMGR update;
+        check_exit_status
+    else
+        printf '\nSkipping repository updates.\n'
+    fi
+    if ask "Would you like to install the apt software updates?" Y; then
+        if [ $PKGMGR=nala ]; then
+            $PKGMGR upgrade -y;
+            check_exit_status
+            $PKGMGR autoremove -y;
+            check_exit_status
+        else
+            $PKGMGR dist-upgrade --allow-downgrades -y;
+            check_exit_status
+            $PKGMGR autoremove -y;
+            check_exit_status
+            $PKGMGR autoclean -y;
+            check_exit_status
+        fi
+    else
+        printf '\nSkipping package upgrades.\n'
+    fi
+}
+
+#Update Snap packages
+UpdateSnap() {
+    if ! CheckForPackage snapd; then
+        if ask "Would you like to update snap packages?" Y; then
+            snap refresh
+            check_exit_status
+        else
+            printf '\nSkipping Snap Update.\n'
+        fi
+    else
+        printf "Snapd is not installed, skipping snap updates.\n"
+    fi
+}
+
+#Update Flatpak packages
+UpdateFlatpak() {
+    if ! CheckForPackage flatpak; then
+        if ask "Would you like to update the Flatpak Packages?" Y; then
+            flatpak update
+            check_exit_status
+        else
+            printf '\nSkipping Flatpak Update.\n'
+        fi
+    else
+        printf "Flatpak is not installed, skipping Flatpak updates.\n"
+    fi
+}
+
+#SetupZSH
+SetupZSH() {
+    if IsRoot; then
+        if CheckForPackage zsh; then
+            if ask "Would you like to setup ZSH?" Y; then
+                $PKGMGR install -y zsh zsh-syntax-highlighting zsh-autosuggestions
+                check_exit_status
+                DefinedSHELL=/bin/zsh
+                usermod --shell $DefinedSHELL root
+                CopyZshrcFile
+            else
+                printf '\nSkipping ZSH Setup.\n'
+            fi
+        fi
+    fi
+    if ask "Would you like to set ZSH as your shell?" Y; then
+        DefinedSHELL=/bin/zsh
+        chsh -s $DefinedSHELL
+        CopyZshrcFile
+    else
+        printf '\nSkipping zsh Setup.\n'
+    fi
+}
+
+#CopyZshrcFile
+CopyZshrcFile() {
+    if IsRoot; then
+        if ask "Would you like to copy the zshrc file included with this script to your home directory?" Y; then
+            rcfile=./rcfiles/zshrc
+            if [[ -f "$rcfile" ]]; then
+                cp ./rcfiles/zshrc /root/.zshrc
+                cp ./rcfiles/zshrc /etc/skel/.zshrc
+            else
+                printf "\nThe zshrc file is not in the expected path. Please run this script from inside the script directory."
+            fi
+        else
+            printf "\nSkipping zshrc file.\n"
+        fi
+    elif ! CheckForPackage zsh; then
+        if ask "Would you like to copy the zshrc file included with this script to your home directory?" Y; then
+            rcfile=./rcfiles/zshrc
+            if [[ -f "$rcfile" ]]; then
+                cp ./rcfiles/zshrc /home/$USER/.zshrc
+            else
+                printf "\nThe zshrc file is not in the expected path. Please run this script from inside the script directory."                
+            fi
+        else
+            printf "\nSkipping zshrc file.\n"
+        fi
+    fi
 }
 
 function banner (){
@@ -49,385 +229,120 @@ function banner (){
         echo -e "$b ║$r                  GitHub  $b: $a www.github.com/d4t4s3c                  $b║"
         echo -e "$b └═════════════════════════════════════════════════════════════════════┘$nc"
         echo ""
-        sleep 4
+        sleep 1
 }
+
+#Functions ---> ^ ^ ^ ^ ^ ^ ^ ^ <-----
+#Script ------> V V V V V V V V <----- 
 
 tput civis
 clear
 checkroot
 clear
 banner
+APTAPPS=
+PKGMGR=apt
+DefinedSHELL=/bin/bash
+if IsRoot; then
+    echo 'export LC_ALL=C.UTF-8' >> /etc/profile
+    echo 'export LANG=C.UTF-8' >> /etc/profile
+fi
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
 
-echo -e "$a update Debian $nc"
-echo ""
-sleep 4
-apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y
-echo ""
-echo -e "$a Installing tools in Debian $nc"
-echo ""
-sleep 4
-echo ""
-echo -e "  [$v$si$b] python"
-echo ""
-sleep 2
-apt-get install -y python
-apt-get install -y python3
-apt-get install -y python-pip
-apt-get install -y python3-pip
-apt-get
-echo ""
-echo -e "  [$v$si$b] ruby"
-echo ""
-sleep 2
-apt install -y ruby
-apt install -y ruby-dev
-echo ""
-echo -e "  [$v$si$b] tcpdump"
-echo ""
-sleep 2
-apt-get install -y tcpdump
-echo ""
-echo -e "  [$v$si$b] nerd fonts"
-echo ""
-sleep 2
+#Setup Nala
+if ! CheckForPackage nala; then
+    PKGMGR=nala
+elif ! CheckForPackage nala-legacy; then
+    PKGMGR=nala
+else
+    if IsRoot; then
+        printf "Nala is a front-end for libapt-pkg with a variety of features such as parallel downloads, clear display of what is happening, and the ability to fetch faster mirrors."
+        if ask "Would you like to install Nala?" N; then
+            SetupNala
+        else
+            printf '\nSkipping Nala Setup.\n'
+        fi
+    else
+        PKGMGR=apt
+    fi
+fi
+
+#update system
+UpdateSoftware
+
+if IsRoot; then
+    if CheckForPackage vim; then
+        if ask "Would you like to install VIM?" Y; then
+            InstallPKG vim
+        else
+            printf '\nSkipping VIM install.\n'
+        fi
+    fi
+fi
+
+if IsRoot; then
+    if CheckForPackage sudo; then 
+        if ask "Would you like to install sudo?" Y; then
+            InstallPKG sudo
+        else
+            printf '\nSkipping sudo setup.\n'
+        fi
+    fi
+fi
+
+printf "\n$a Installing tools in Debian $nc\n"
+sleep 1
+printf "\n  [$v$si$b] nerd fonts\n"
+sleep 1
 cd /usr/local/share/fonts
 mv /root/DebianToolkit/box/Hack.zip .
 unzip Hack.zip
 rm -rf Hack.zip
 cd /root/DebianToolkit/
-echo ""
-echo -e "  [$v$si$b] hash-id"
-echo ""
-sleep 2
+printf "\n  [$v$si$b] hash-id\n"
+sleep 1
 cd box
 git clone https://github.com/blackploit/hash-identifier.git
 cd hash-identifier
 chmod +x hash-id.py
 cd ..
 cd ..
-echo ""
-echo -e "  [$v$si$b] zsh"
-echo ""
-sleep 2
-apt-get install -y zsh
-echo ""
-echo -e "  [$v$si$b] zsh-autosuggestions"
-echo ""
-sleep 2
-apt-get install -y zsh-autosuggestions
-echo ""
-echo -e "  [$v$si$b] zsh-syntax-highlighting"
-echo ""
-sleep 2
-apt-get install -y zsh-syntax-highlighting
-echo ""
-echo -e "  [$v$si$b] powerlevel10k"
-echo ""
-sleep 2
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
-usermod --shell /usr/bin/zsh root
-echo ""
-echo -e "  [$v$si$b] rlwrap"
-echo ""
-sleep 2
-apt-get install -y rlwrap
-echo ""
-echo -e "  [$v$si$b] searchsploit"
-echo ""
-sleep 2
-apt-get install -y exploitdb
-apt-get install -y exploitdb-bin-sploits exploitdb-papers
+printf "\n  [$v$si$b] exploitdb\n"
+exploitdb
+exploitdb-bin-sploits
+exploitdb-papers
 git clone https://github.com/offensive-security/exploitdb.git /opt/exploitdb
 sed 's|path_array+=(.*)|path_array+=("/opt/exploitdb")|g' /opt/exploitdb/.searchsploit_rc > ~/.searchsploit_rc
 ln -sf /opt/exploitdb/searchsploit /usr/local/bin/searchsploit
-echo ""
-echo -e "  [$v$si$b] snapd"
-echo ""
-sleep 2
-apt install snapd
-echo ""
-echo -e "  [$v$si$b] whatweb"
-echo ""
-sleep 2
-apt-get install -y whatweb
-echo ""
-echo -e "  [$v$si$b] john the ripper"
-echo ""
-sleep 2
-snap install john-the-ripper
-echo ""
-echo -e "  [$v$si$b] xterm"
-echo ""
-sleep 2
-apt-get install -y xterm
-echo ""
-echo -e "  [$v$si$b] aircrack-ng"
-echo ""
-sleep 2
-apt-get install -y aircrack-ng 
-echo ""
-echo -e "  [$v$si$b] guest additions (virtualbox)"
-echo ""
-sleep 2
-apt-get install -y build-essential dkms
-cd box
-chmod +x VBoxLinuxAdditions.run
-./VBoxLinuxAdditions.run
 echo ""     
-echo -e "  [$v$si$b] metasploit"
-echo ""
-sleep 2
+echo -e "  [$v$si$b] metasploit\n"
+sleep 1
 wget https://downloads.metasploit.com/data/releases/metasploit-latest-linux-x64-installer.run
 chmod +x metasploit-latest-linux-x64-installer.run
 xterm -hold -e "./metasploit-latest-linux-x64-installer.run" &
 cd ..
-echo ""
-echo -e "  [$v$si$b] wordlist rockyou"
-echo ""
-sleep 2
+printf "\n  [$v$si$b] wordlist rockyou\n"
+sleep 1
 cd wordlist
 wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
 cd ..
-echo ""                    
-echo -e "  [$v$si$b] netcat"
-echo ""
-sleep 2
-apt-get install -y netcat
 echo ""             
-echo -e "  [$v$si$b] sublime text"
-echo ""
-sleep 2
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install sublime-text
-echo ""
-echo -e "  [$v$si$b] terminator"
-echo ""
-sleep 2
-apt-get install -y terminator
-echo ""           
-echo -e "  [$v$si$b] nmap"
-echo ""
-sleep 2
-apt-get install -y nmap
-echo ""           
-echo -e "  [$v$si$b] hydra"
-echo ""
-sleep 2
-apt-get install -y hydra
-echo ""                    
-echo -e "  [$v$si$b] ftp"
-echo ""
-sleep 2
-apt-get install -y ftp
-echo ""                    
-echo -e "  [$v$si$b] putty"
-echo ""
-sleep 2
-apt-get install -y putty
-echo ""
-sleep 2                       
-echo -e "  [$v$si$b] zenity"
-echo ""
-sleep 2
-apt-get install -y zenity
-echo ""               
-echo -e "  [$v$si$b] netdiscover"
-echo ""
-sleep 2
-apt-get install -y netdiscover
-echo ""                
-echo -e "  [$v$si$b] net-tools"
-echo ""
-sleep 2
-apt-get install -y net-tools
-echo ""             
-echo -e "  [$v$si$b] wine"
-echo ""
-sleep 2
-apt-get install -y wine
-dpkg --add-architecture i386 && apt-get install -y wine32
-apt-get install -y wine32
+echo -e "  [$v$si$b] wine\n"
+sleep 1
+InstallPKG wine
+dpkg --add-architecture i386 && $PKGMGR install -y wine32
+$PKGMGR install -y wine32
 echo ""                      
-echo -e "  [$v$si$b] angry ip scanner"
-echo ""
-sleep 2
+echo -e "  [$v$si$b] angry ip scanner\n"
+sleep 1
 cd box
 dpkg -i ipscan_3.6.2_amd64.deb
 cd ..
-echo ""              
-echo -e "  [$v$si$b] set"
-echo ""
-sleep 2
-apt-get install -y set
-echo ""    
-echo -e "  [$v$si$b] apache2"
-echo ""
-sleep 2
-apt-get install -y apache2
-echo ""
-echo -e "  [$v$si$b] dirbuster"
-echo ""
-sleep 2
-apt-get install -y dirbuster
-echo ""
-echo -e "  [$v$si$b] wpscan"
-echo ""
-sleep 2
-apt-get install -y wpscan
-echo ""
-echo -e "  [$v$si$b] locate"
-echo ""
-sleep 2
-apt-get install -y locate
-updatedb
-echo ""
-echo -e "  [$v$si$b] cadaver"
-echo ""
-sleep 2
-apt-get install -y cadaver
-echo ""
-echo -e "  [$v$si$b] recon-ng"
-echo ""
-sleep 2
-apt-get install -y recon-ng
-echo ""
-echo -e "  [$v$si$b] curl"
-echo ""
-sleep 2
-apt-get install -y curl
-echo ""
-echo -e "  [$v$si$b] dmitry"
-echo ""
-sleep 2
-apt-get install -y dmitry
-echo ""
-echo -e "  [$v$si$b] crunch"
-echo ""
-sleep 2
-apt-get install -y crunch
-echo ""
-echo -e "  [$v$si$b] masscan"
-echo ""
-sleep 2
-apt-get install -y masscan
-echo ""
-echo -e "  [$v$si$b] sqlmap"
-echo ""
-sleep 2
-apt-get install -y sqlmap
-echo ""
-echo -e "  [$v$si$b] putty"
-echo ""
-sleep 2
-apt-get install -y putty
-echo ""
-echo -e "  [$v$si$b] wfuzz"
-echo ""
-sleep 2
-apt-get install -y wfuzz
-echo ""
-echo -e "  [$v$si$b] wireshark"
-echo ""
-sleep 2
-apt-get install -y wireshark
-echo ""
-echo -e "  [$v$si$b] dirb"
-echo ""
-sleep 2
-apt-get install -y dirb
-echo -e "  [$v$si$b] nikto"
-echo ""
-apt-get install nikto
-echo ""
-sleep 2
-echo -e "  [$v$si$b] patator"
-echo ""
-sleep 2
-apt-get install -y patator
-echo "" 
-echo -e "  [$v$si$b] medusa"
-echo ""
-sleep 2
-apt-get install -y medusa
-echo ""
-echo -e "  [$v$si$b] cewl"
-echo ""
-sleep 2
-apt-get install -y cewl
-echo "" 
-echo -e "  [$v$si$b] openvpn"
-echo ""
-sleep 2
-apt-get install -y openvpn
-echo ""
-echo -e "  [$v$si$b] nomacs"
-echo ""
-sleep 2
-apt-get install -y nomacs
-echo ""
-echo -e "  [$v$si$b] maltego"
-echo ""
-sleep 2
-apt-get install -y maltego
-echo ""
-sleep 2
-echo -e "  [$v$si$b] smbmap"
-echo ""
-sleep 2
-apt-get install -y smbmap
-echo ""
-echo -e "  [$v$si$b] smbclient"
-echo ""
-sleep 2
-apt-get install -y smbclient
-echo ""
-echo -e "  [$v$si$b] enum4linux"
-echo ""
-sleep 2
-apt-get install -y enum4linux
-echo ""
-echo -e "  [$v$si$b] lsd"
-echo ""
-sleep 2
+printf "\n  [$v$si$b] lsd\n"
+sleep 1
 cd box
 dpkg -i lsd_0.14.0_amd64.deb
 cd ..
-echo ""
-echo -e "  [$v$si$b] kismet"
-echo ""
-sleep 2
-apt-get install -y kismet
-echo ""
-echo -e "  [$v$si$b] simple screen recorder"
-echo ""
-sleep 2
-apt-get install -y simplescreenrecorder
-echo ""
-echo -e "  [$v$si$b] custom .p10k.zsh"
-echo ""
-sleep 2
-cd /root/DebianToolkit/
-cp .p10k.zsh /root
-cd ..
-echo ""
-echo -e "  [$v$si$b] custom .bashrc"
-echo ""
-sleep 2
-mv .bashrc .bashrcBACKUP
-cd DebianToolkit
-cp .bashrc /root
-echo ""
-echo -e "  [$v$si$b] add wallpapers"
-cd wallpapers
-cp * /usr/share/images/desktop-base/
-cd ..
-echo ""
-echo -e "$a Installation completed $nc"
-sleep 5
-echo ""
 tput cnorm
 exit
